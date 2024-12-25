41bf5926ddfdc5707c3744729ded8ce21b951ed7
FilipN2003 <filip.nikolic2303@gmail.com>
2024-12-25 18:16:47 +0100

    Dodata klasa RenderLoop

---
+++ src/RenderLoop.cpp
@@ -0,0 +1,55 @@
+
+#include "RenderLoop.h"
+
+RenderLoop::RenderLoop(const Window& win, bool showFps, bool vSync) :
+    window(win),
+    renderTimer(RenderTimer(showFps, vSync)),
+    pauseSimulation(true)
+{}
+
+RenderLoop::RenderLoop()=default;
+
+void RenderLoop::runLoop(ParticleSimulation *particleSimulation) {
+    this->pauseSimulation = true;
+
+    while (!glfwWindowShouldClose(this->window.getWindow()))
+    {
+
+        this->renderTimer.updateTime(this->window, this->pauseSimulation);
+
+        if(!this->pauseSimulation){
+            particleSimulation->update();
+        }
+
+        particleSimulation->draw();
+
+        // ======================
+        // Swap buffers: Front buffer(render) and back buffer (next render)
+        glfwSwapBuffers(this->window.getWindow());
+        // ======================
+
+        // ======================
+        // Checks if any events are triggered (keys pressed/released, mouse moved etc.) and calls the corresponding functions
+        glfwPollEvents();
+        // ======================
+
+    }
+}
+
+void RenderLoop::setPauseSimulation(bool pause) {
+    this->pauseSimulation = pause;
+}
+
+bool RenderLoop::getPauseSimulation(){
+    return this->pauseSimulation;
+}
+
+int RenderLoop::getIteration() {
+    return this->renderTimer.getIteration();
+}
+
+
+RenderLoop::~RenderLoop(){
+    this->renderTimer.printFinalStats();
+}
+
